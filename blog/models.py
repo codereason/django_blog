11ddd48daae0b603a8db882ec02ed5fe30f0a4dc
yuhuang1996 <sdu_huangyu@foxmail.com>
2020-01-25 13:43:47 +0800

    增加文章阅读量字段

--- blog/models.py
+++ blog/models.py
@@ -37,6 +37,7 @@ class Post(models.Model):
     category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
     author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+    views = models.PositiveIntegerField(default=0, editable=False)
 
     def save(self, *args, **kwargs):
         md = markdown.Markdown(extensions=[
